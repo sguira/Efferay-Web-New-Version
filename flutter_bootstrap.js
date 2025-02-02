184093f7f98d8c456423e1d87f436393a8da6328
sguira <stage_nlp3@smarte-conseil.com>
2025-02-02 08:52:22 +0000

    mise a jour

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1875474156"
+    serviceWorkerVersion: "3841066942"
   }
 });
