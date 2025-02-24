217b0c54e22e27aaa2200eea11243ed594bf1568
sguira <stage_nlp3@smarte-conseil.com>
2025-02-24 23:44:20 +0000

    ajout des footer

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1994836542"
+    serviceWorkerVersion: "1032585515"
   }
 });
